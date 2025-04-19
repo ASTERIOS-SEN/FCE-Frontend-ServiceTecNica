221f18c7380256a4911c50f635705f99477cdf6a
dsevilla <dsevilla@vitalpbx.com>
2025-04-18 19:14:46 -0600

    feat: add .gitignore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,7 @@ export default defineNuxtConfig({
   css: ['@/assets/css/tailwind.css'],
   build: {
     transpile: ['@headlessui/vue']
-  }
+  },
+  compatibilityDate: '2025-04-18',
+  modules: ['@nuxtjs/tailwindcss']
 })
